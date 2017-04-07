3e3741d17c9d515ce231354bcb1298b5095f32e7
n8dgr8 <nathan@kwintek.net>
2017-04-07 14:32:31 -0500

    Updated makezip

--- makezip.sh
+++ makezip.sh
@@ -4,4 +4,4 @@ pushd ~/.virtualenvs/env/lib/python2.7/site-packages
 zip -ur ~/src/satstalker_predictor/satstalker_predictor.zip .
 
 popd
-zip -u satstalker_predictor.zip satstalker_predictor.py awesome_satellites.json
+zip -u satstalker_predictor.zip satstalker_predictor.py awesome_satellites.json SatStalkerPredictor.json
