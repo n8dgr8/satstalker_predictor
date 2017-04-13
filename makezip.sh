cddf015ccbf3a0c703fdaae1a594baed5c5070fb
n8dgr8 <nathan@kwintek.net>
2017-04-13 10:27:36 -0500

    Improvements, mostly good ones too

--- makezip.sh
+++ makezip.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-pushd ~/.virtualenvs/env/lib/python2.7/site-packages
+pushd ~/.virtualenvs/satstalker_predictor/lib/python2.7/site-packages
 zip -ur ~/src/satstalker_predictor/satstalker_predictor.zip .
 
 popd
-zip -u satstalker_predictor.zip satstalker_predictor.py awesome_satellites.json SatStalkerPredictor.json
+zip -u satstalker_predictor.zip satstalker_predictor.py awesome_satellites.json
--- satstalker_predictor.py
+++ satstalker_predictor.py
@@ -11,6 +11,17 @@ def publish_sqs(message):
     response = queue.send_message(MessageBody=json.dumps(message))
     print response
 
+def publish_sns(message):
+    sns = boto3.resource('sns')
+
+    topic = sns.Topic('arn:aws:sns:us-east-1:406316711618:satstalker-predictor')
+
+    response = topic.publish(
+        Message=json.dumps(message)
+    )
+
+    print response
+
 def lambda_handler(event, context):
     tle_path = '/tmp/tle'
 
@@ -24,24 +35,18 @@ def lambda_handler(event, context):
     for satellite in awesome_satellites:
         sat_orbital = orbital.Orbital(satellite['name'], tle_file=tle_path)
 
-        print '%s @ %s' % (satellite['name'], satellite['frequency'])
-        print '-------'
         next_passes = sat_orbital.get_next_passes(now, 24, event['lon'], event['lat'], event['alt'])
 
         for next_pass in next_passes:
             (start, end, maximum) = next_pass
 
-            publish_sqs(
+            publish_sns(
                 {
                     'satellite': satellite,
-                    'pass_begin': start.strftime('%Y-%m-%d %H:%M:%S'),
-                    'pass_end': end.strftime('%Y-%m-%d %H:%M:%S')
+                    'pass_begin': start.isoformat(),
+                    'pass_end': end.isoformat()
                 }
             )
-
-            print '%02d:%02d:%02d' % (start.hour, start.minute, start.second)
-            print '%02d:%02d:%02d' % (maximum.hour, maximum.minute, maximum.second)
-            print '%02d:%02d:%02d' % (end.hour, end.minute, end.second)
 
 def main():
     dsm_lat = 41.6005448
